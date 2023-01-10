1f9a9213d3b039542bbf6422221ece03dbb69247
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-10 20:43:55 +0100

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Philipp Sanktjohanser on 10.01.23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
